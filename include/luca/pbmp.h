35e8334972c7d332c99a8bbdaf3f45e1d2f8c051
pos <pgousdal@users.noreply.github.com>
2026-09-26 09:15:19 +0200

    PBMP: add secret-safe config introspection

--- include/luca/pbmp.h
+++ include/luca/pbmp.h
@@ -5,7 +5,7 @@
 #include <time.h>
 #include "luca/command_queue.h"
 #include "luca/log.h"
-struct luca_pbmp_state { const char *nick; const char *network; const char *channel; volatile int connected; volatile int channel_state; struct luca_command_queue *commands; const char *module_id; volatile int module_active; uint64_t rx_lines; uint64_t tx_lines; uint64_t reconnects; time_t session_started; struct luca_log *log; };
+struct luca_pbmp_state { const char *nick; const char *network; const char *channel; volatile int connected; volatile int channel_state; struct luca_command_queue *commands; const char *module_id; volatile int module_active; uint64_t rx_lines; uint64_t tx_lines; uint64_t reconnects; time_t session_started; struct luca_log *log; const char *port; const char *user; const char *realname; const char *script; int tls; int sasl; int reconnect_seconds; };
 int luca_pbmp_response(const char *request,const struct luca_pbmp_state *,char *out,size_t outsz);
 int luca_pbmp_serve(const char *socket_path,const struct luca_pbmp_state *);
 #endif

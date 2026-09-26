895d4fe2efd93d8994bc0a8a4d068cfa64b04dd8
pos <pgousdal@users.noreply.github.com>
2026-09-26 10:32:45 +0200

    M2: replace shared PBMP state with C11 atomics

--- include/luca/pbmp.h
+++ include/luca/pbmp.h
@@ -3,9 +3,10 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <time.h>
+#include <stdatomic.h>
 #include "luca/command_queue.h"
 #include "luca/log.h"
-struct luca_pbmp_state { const char *nick; const char *network; const char *channel; volatile int connected; volatile int channel_state; struct luca_command_queue *commands; const char *module_id; volatile int module_active; uint64_t rx_lines; uint64_t tx_lines; uint64_t reconnects; time_t session_started; struct luca_log *log; const char *port; const char *user; const char *realname; const char *script; int tls; int sasl; int reconnect_seconds; };
+struct luca_pbmp_state { const char *nick; const char *network; const char *channel; atomic_int connected; atomic_int channel_state; struct luca_command_queue *commands; const char *module_id; atomic_int module_active; atomic_uint_fast64_t rx_lines; atomic_uint_fast64_t tx_lines; atomic_uint_fast64_t reconnects; atomic_long session_started; struct luca_log *log; const char *port; const char *user; const char *realname; const char *script; int tls; int sasl; int reconnect_seconds; };
 int luca_pbmp_response(const char *request,const struct luca_pbmp_state *,char *out,size_t outsz);
 int luca_pbmp_serve(const char *socket_path,const struct luca_pbmp_state *);
 #endif
